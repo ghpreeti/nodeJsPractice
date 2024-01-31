ae7ac19158cbe2296c2171b895994e45d2f72d8e
Preeti Singh <115339754+ghpreeti@users.noreply.github.com>
2024-01-31 11:42:05 +0530

    Add files via upload

---
+++ uc.js
@@ -0,0 +1,9 @@
+// var express = require('express');
+var http = require("http");
+//var uc = require("upper-case");
+
+http.createServer(function(req,res){
+ // res.writeHead(200, {'Content-Type': 'text/html'});
+ // res.write(uc.upperCase("Hello World!"));
+  res.end("hi");
+}).listen(3000);
